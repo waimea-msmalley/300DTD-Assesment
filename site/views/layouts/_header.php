942a018ace9368bfddeed3a5640f86014cca1366
Myles Smalley <158526601+waimea-msmalley@users.noreply.github.com>
2024-08-15 12:04:26 +1200

    edited header

--- site/views/layouts/_header.php
+++ site/views/layouts/_header.php
@@ -1,13 +1,15 @@
 <!-- Header typically has the site name which links to home page -->
 
 <header id="main-header">
+
+    <a href="/home"><?= SITE_NAME ?></a>
     
     <?php require '_nav.php'; ?>
 
     <?php if ($loggedIn) {
     echo '<p>Hello, ' . $_SESSION['user']['forename'] . '</p>';
-    echo  '<a href="/home"><?= SITE_NAME ?></a>';
-    }
+}
+
 ?>
 
 </header>
